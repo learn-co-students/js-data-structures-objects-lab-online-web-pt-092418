00bdc52d9e13ca113c4f92e0ef1934b3b84f487c
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-22 12:56:01 -0400

    Interim checkin

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution in this file!
+const driver = {name: "Sam"};
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const newDriver = Object.assigm({}, driver);
+  newDriver["address"] = "11 Broadway"
+  };
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver["address"] = "12 Broadway";
+};
